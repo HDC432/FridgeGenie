634e2fa2e4b7083419a512457a9049a568316d97
graceliu369 <116697459+graceliu369@users.noreply.github.com>
2025-04-29 17:24:31 -0400

    Delete ai directory

--- ai/openai_client.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigKeys, env } from '../app/config';
-import { OpenAI } from 'openai';
-
-export const getOpenAiClient = () => {
-  return new OpenAI({
-    apiKey: env.getConfig(ConfigKeys.OPENAI_API_KEY)!,
-  });
-};
